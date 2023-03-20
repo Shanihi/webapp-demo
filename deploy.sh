e6497af2cf357acd5ba6023c069511057a28fcc5
Shan Hu <shan.hu@avanade.com>
2023-03-20 08:23:23 +0100

    x

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "Deploying your resources"
 az deployment sub create --template-file ./main.bicep --parameters './parameters.dev.json' --location westeurope --output jsonc
 
 #deploy ACR. Assign ACR name and SKU  
 cd registries
+echo "Deploying your Azure Container Registry"
 az deployment group create -g demo-app-service-rg --template-file acr.bicep --parameters acrName=shanBicepContainerRegistry acrSku=Basic
--- publish.sh
+++ publish.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 CURRENTDATE=$(date +%Y%m%d%H%M%S)
 VERSION="1.0_$CURRENTDATE"
-ACRNAME="shanBicepContainerRegistry"
 
 
 echo "Publish App Service Plan $VERSION"
@@ -12,3 +11,4 @@ az bicep publish -f ./modules/sqlDatabase.bicep -t br:shanBicepContainerRegistry
 
 echo "Publish Web App $VERSION"
 az bicep publish -f ./modules/webApp.bicep -t br:shanBicepContainerRegistry.azurecr.io/webapp:$VERSION
+
