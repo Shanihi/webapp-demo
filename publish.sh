23fa8ccc450a93ba460d02d0601b0a2f2172ec18
Shan Hu <shan.hu@avanade.com>
2023-03-27 08:50:49 +0200

    test push

--- publish.sh
+++ publish.sh
@@ -4,16 +4,16 @@ VERSION="1.0_$CURRENTDATE"
 
 
 echo "Publish App Service Plan $VERSION"
-az bicep publish -f ./modules/appServicePlan.bicep -t br:shanBicepContainerRegistryTest.azurecr.io/appserviceplan:$VERSION
-az bicep publish -f ./modules/appServicePlan.bicep -t br:shanBicepContainerRegistryTest.azurecr.io/appserviceplan:latest
+az bicep publish -f ./modules/appServicePlan.bicep -t br:avanadeBicepContainerRegistry.azurecr.io/appserviceplan:$VERSION
+az bicep publish -f ./modules/appServicePlan.bicep -t br:avanadeBicepContainerRegistry.azurecr.io/appserviceplan:latest
 
 echo "Publish SQL Database $VERSION"
-az bicep publish -f ./modules/sqlDatabase.bicep -t br:shanBicepContainerRegistryTest.azurecr.io/sqldatabase:$VERSION
-az bicep publish -f ./modules/sqlDatabase.bicep -t br:shanBicepContainerRegistryTest.azurecr.io/sqldatabase:latest
+az bicep publish -f ./modules/sqlDatabase.bicep -t br:avanadeBicepContainerRegistry.azurecr.io/sqldatabase:$VERSION
+az bicep publish -f ./modules/sqlDatabase.bicep -t br:avanadeBicepContainerRegistry.azurecr.io/sqldatabase:latest
 
 echo "Publish Web App $VERSION"
-az bicep publish -f ./modules/webApp.bicep -t br:shanBicepContainerRegistryTest.azurecr.io/webapp:$VERSION
-az bicep publish -f ./modules/webApp.bicep -t br:shanBicepContainerRegistryTest.azurecr.io/webapp:latest
+az bicep publish -f ./modules/webApp.bicep -t br:avanadeBicepContainerRegistry.azurecr.io/webapp:$VERSION
+az bicep publish -f ./modules/webApp.bicep -t br:avanadeBicepContainerRegistry.azurecr.io/webapp:latest
 
 
 
