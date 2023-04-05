ebc2998b8d61203a88be0f1253f71c9f19558bb6
Shan Hu <shan.hu@avanade.com>
2023-04-05 14:45:30 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-echo "Deploying your resources"
-az deployment group create --resource-group fakeClientTestRG --template-file ./main.bicep --parameters './parameters.dev.json' --output jsonc
-
 #deploy ACR. Assign ACR name and SKU  
 cd registries
 echo "Deploying your Azure Container Registry"
