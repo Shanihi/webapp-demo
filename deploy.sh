73023a04b8fcd1abb003b44b31d526e44dbf283f
Shan Hu <shan.hu@avanade.com>
2023-04-20 11:29:26 +0200

    test reusable

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@
 cd registries
 echo "Deploying your Azure Container Registry"
 az deployment group create --resource-group myAvanadeACR --template-file acr.bicep --parameters acrName=shanBicepContainerRegistryTest acrSku=Basic
+
+
